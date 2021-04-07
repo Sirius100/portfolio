dace7a3e002e3d95f8bf6f4d3016a677284f43cf
ESA <esaul-00@mail.ru>
2021-04-07 20:57:07 +0300

    create header

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+  <head>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="src/css/style.css">
+    <!--link(rel='preconnect' href='https://fonts.gstatic.com')-->
+    <!--link(href='https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap' rel='stylesheet')-->
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="title" content="Обзор новых возможностей">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit-no">
+    <meta name="robots" content="noindex">
+    <meta name="Author" content="ФИО автора">
+    <meta name="Copyright" content="кому принадлежат авторские права">
+    <meta name="Address" content="адрес где создавался контент">
+    <title>Portfolio</title>
+  </head>
+  <body>
+    <div class="container-fuild">
+      <div class="container">
+        <div class="row mx-0 justify-content-end my-2">
+          <ul class="nav nav-tabs">
+            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link text-warning" href="#">Portfolio</a></li>
+            <li class="nav-item"><a class="nav-link text-warning" href="#">Contant</a></li>
+            <li class="nav-item"><a class="nav-link text-warning" href="#" tabindex="-1" aria-disabled="true">Disabled</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <script src="gulp/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../js/menu.js"></script>
+  </body>
+</html>
