e3747b246aba06b35a3f94cf6a325d1814ab19fd
ESA <esaul-00@mail.ru>
2021-04-08 14:22:21 +0300

    100% header

--- src/php/index.php
+++ src/php/index.php
@@ -20,18 +20,25 @@
     <div class="container-fuild">
       <div class="container">
         <div class="row mx-0 justify-content-end my-2">
-          <ul class="nav nav-tabs">
-            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
-            <li class="nav-item"><a class="nav-link text-warning" href="#">Portfolio</a></li>
-            <li class="nav-item"><a class="nav-link text-warning" href="#">Contant</a></li>
-            <li class="nav-item"><a class="nav-link text-warning" href="#" tabindex="-1" aria-disabled="true">Disabled</a></li>
-          </ul>
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active"><a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Portfolio</a></li>
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact</a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown"><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div><a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
       </div>
     </div>
     <script src="gulp/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../../js/menu.js"></script>
   </body>
 </html>
