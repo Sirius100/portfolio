603939adbf8535f63b4f580060a8154c1b9b5e62
ESA <esaul-00@mail.ru>
2021-04-16 20:02:36 +0300

    90% create contact desctop-mobile

--- src/php/index.php
+++ src/php/index.php
@@ -17,6 +17,7 @@
     <title>Portfolio</title>
   </head>
   <body>
+    <!-- 1-->
     <div class="container-fuild">
       <div class="container">
         <div class="row mx-0 justify-content-end my-2">
@@ -37,12 +38,13 @@
         </div>
       </div>
     </div>
+    <!-- 2-->
     <div class="container-fuild">
       <div class="container">
         <div class="row mx-0 h-100 align-items-center">
           <div class="col-sm-12 col-md-5 home_foto"></div>
           <div class="col-sm-12 col-md-7 home_studio">
-            <p class="lead">Студия разработки<br><span>Leading</span></p>
+            <p class="lead">Студия WEB разработки<br><span>Leading</span></p>
             <p class="lead text-left">Веб-студия “LEADING” - это команда программистов, дизайнеров, копирайтеров, специалистов по продвижению, менеджеров, которые занимаются веб-разработкой и созданием Телеграм-ботов в Вологде и по всей России. Уже не новость, что сайт - это не только рабочий инструмент, но и показатель статуса компании, его деловой костюм, если хотите. Поэтому наша миссия  помочь вашему бизнесу, а наша приоритетная задача по проекту - создать сайт, который украсит наше портфолио.</p>
             <p class="lead mb-0">
               <button class="btn btn-secondary btn-lg">Оставить заявку</button><br><span>Мы открыты для плодотворного сотрудничества!</span>
@@ -51,13 +53,14 @@
         </div>
       </div>
     </div>
+    <!--	3-->
     <div class="container-fuild">
       <div class="container">
         <section class="row mx-0 my-2 align-items-center flex-column">
-          <h6 class="display-6">Наши качества</h6>
+          <h6 class="display-6">Наши помощники</h6>
           <h5 class="display-5">Креативное мышление и современные технологии</h5>
-          <div class="row mx-0 justify-content-around px-5">
-            <div class="card">
+          <div class="row justify-content-between">
+            <div class="card col-12">
               <div class="card-body">
                 <svg width="55" height="55" viewbox="-10 0 80 60" baseProfile="full">
                   <g>
@@ -76,7 +79,7 @@
               <h5 class="display-5">Креативность</h5>
               <p class="lead col-12 col-sm-10 px-0">Творческие способности — способности человека принимать творческие решения, понимать, принимать и создавать принципиально новые идеи</p>
             </div>
-            <div class="card">
+            <div class="card col-12">
               <div class="card-body">
                 <svg width="55" height="55" viewbox="-10 0 80 60" baseProfile="full">
                   <path d="M59.972,20.164c0.007-0.041,0.011-0.081,0.013-0.123c0.002-0.049,0.001-0.096-0.003-0.145  c-0.004-0.042-0.01-0.082-0.02-0.123c-0.011-0.048-0.026-0.094-0.044-0.14c-0.009-0.024-0.011-0.049-0.022-0.072  c-0.008-0.017-0.022-0.03-0.031-0.047c-0.009-0.017-0.013-0.035-0.023-0.052l-10-16c-0.046-0.073-0.1-0.139-0.161-0.197  c-0.001-0.001-0.002-0.001-0.003-0.002c-0.086-0.081-0.188-0.139-0.297-0.185c-0.014-0.006-0.023-0.018-0.038-0.024  c-0.011-0.004-0.023-0.002-0.034-0.005c-0.101-0.034-0.205-0.057-0.314-0.057h-38c-0.108,0-0.211,0.023-0.31,0.056  c-0.011,0.003-0.022,0.001-0.032,0.005c-0.015,0.005-0.024,0.017-0.038,0.023c-0.104,0.043-0.202,0.099-0.287,0.175  c-0.005,0.005-0.012,0.008-0.017,0.012c-0.062,0.058-0.117,0.125-0.163,0.198l-10,16c-0.01,0.016-0.014,0.035-0.023,0.052  c-0.009,0.017-0.023,0.03-0.031,0.047c-0.011,0.023-0.013,0.048-0.023,0.072c-0.018,0.046-0.033,0.092-0.044,0.14  c-0.009,0.041-0.016,0.082-0.02,0.124C0,19.945-0.001,19.993,0.001,20.041c0.002,0.042,0.006,0.082,0.013,0.123  c0.008,0.048,0.021,0.095,0.037,0.142c0.013,0.039,0.027,0.077,0.045,0.115c0.009,0.018,0.012,0.038,0.022,0.056  c0.016,0.029,0.04,0.051,0.058,0.078c0.014,0.021,0.022,0.044,0.039,0.064l28.989,35.986c0.002,0.003,0.004,0.005,0.006,0.007  l0.005,0.007c0.003,0.004,0.008,0.005,0.011,0.009c0.082,0.099,0.184,0.174,0.295,0.234c0.03,0.016,0.058,0.03,0.089,0.043  c0.121,0.051,0.249,0.087,0.383,0.087c0.114,0,0.229-0.02,0.343-0.061c0.017-0.006,0.028-0.02,0.045-0.027  c0.051-0.021,0.091-0.055,0.137-0.084c0.076-0.048,0.147-0.098,0.206-0.163c0.014-0.015,0.035-0.022,0.048-0.038l29-36  c0.016-0.02,0.024-0.044,0.039-0.064c0.019-0.027,0.042-0.049,0.058-0.078c0.01-0.018,0.013-0.038,0.022-0.056  c0.018-0.038,0.032-0.075,0.045-0.115C59.95,20.259,59.964,20.213,59.972,20.164z M32.095,4.993h15.459l-4.86,13.045L32.095,4.993z   M40.892,18.993H19.095L29.993,5.58L40.892,18.993z M17.222,18.125L12.423,4.993h15.469L17.222,18.125z M41.593,20.993  L29.818,52.599L18.269,20.993H41.593z M49.221,6.246l7.967,12.748H44.472L49.221,6.246z M10.756,6.259l4.653,12.734H2.798  L10.756,6.259z M16.14,20.993l10.839,29.664L3.083,20.993H16.14z M32.39,51.423l11.337-30.43h13.176L32.39,51.423z"></path>
@@ -85,7 +88,7 @@
               <h5 class="display-5">Технологии</h5>
               <p class="lead col-12 col-sm-10 px-0">Отлично зарекомендовавшие себя frameworks, удобные и надежные CMS и базы данных.</p>
             </div>
-            <div class="card">
+            <div class="card col-12">
               <div class="card-body">
                 <svg width="55" height="55" viewbox="-10 0 80 60" baseProfile="full">
                   <path d="M55.014,45.389l-9.553-4.776C44.56,40.162,44,39.256,44,38.248v-3.381c0.229-0.28,0.47-0.599,0.719-0.951  c1.239-1.75,2.232-3.698,2.954-5.799C49.084,27.47,50,26.075,50,24.5v-4c0-0.963-0.36-1.896-1-2.625v-5.319  c0.056-0.55,0.276-3.824-2.092-6.525C44.854,3.688,41.521,2.5,37,2.5s-7.854,1.188-9.908,3.53c-1.435,1.637-1.918,3.481-2.064,4.805  C23.314,9.949,21.294,9.5,19,9.5c-10.389,0-10.994,8.855-11,9v4.579c-0.648,0.706-1,1.521-1,2.33v3.454  c0,1.079,0.483,2.085,1.311,2.765c0.825,3.11,2.854,5.46,3.644,6.285v2.743c0,0.787-0.428,1.509-1.171,1.915l-6.653,4.173  C1.583,48.134,0,50.801,0,53.703V57.5h14h2h44v-4.043C60,50.019,58.089,46.927,55.014,45.389z M14,53.262V55.5H2v-1.797  c0-2.17,1.184-4.164,3.141-5.233l6.652-4.173c1.333-0.727,2.161-2.121,2.161-3.641v-3.591l-0.318-0.297  c-0.026-0.024-2.683-2.534-3.468-5.955l-0.091-0.396l-0.342-0.22C9.275,29.899,9,29.4,9,28.863v-3.454  c0-0.36,0.245-0.788,0.671-1.174L10,23.938l-0.002-5.38C10.016,18.271,10.537,11.5,19,11.5c2.393,0,4.408,0.553,6,1.644v4.731  c-0.64,0.729-1,1.662-1,2.625v4c0,0.304,0.035,0.603,0.101,0.893c0.027,0.116,0.081,0.222,0.118,0.334  c0.055,0.168,0.099,0.341,0.176,0.5c0.001,0.002,0.002,0.003,0.003,0.005c0.256,0.528,0.629,1,1.099,1.377  c0.005,0.019,0.011,0.036,0.016,0.054c0.06,0.229,0.123,0.457,0.191,0.68l0.081,0.261c0.014,0.046,0.031,0.093,0.046,0.139  c0.035,0.108,0.069,0.215,0.105,0.321c0.06,0.175,0.123,0.356,0.196,0.553c0.031,0.082,0.065,0.156,0.097,0.237  c0.082,0.209,0.164,0.411,0.25,0.611c0.021,0.048,0.039,0.1,0.06,0.147l0.056,0.126c0.026,0.058,0.053,0.11,0.079,0.167  c0.098,0.214,0.194,0.421,0.294,0.621c0.016,0.032,0.031,0.067,0.047,0.099c0.063,0.125,0.126,0.243,0.189,0.363  c0.108,0.206,0.214,0.4,0.32,0.588c0.052,0.092,0.103,0.182,0.154,0.269c0.144,0.246,0.281,0.472,0.414,0.682  c0.029,0.045,0.057,0.092,0.085,0.135c0.242,0.375,0.452,0.679,0.626,0.916c0.046,0.063,0.086,0.117,0.125,0.17  c0.022,0.029,0.052,0.071,0.071,0.097v3.309c0,0.968-0.528,1.856-1.377,2.32l-2.646,1.443l-0.461-0.041l-0.188,0.395l-5.626,3.069  C15.801,46.924,14,49.958,14,53.262z M58,55.5H16v-2.238c0-2.571,1.402-4.934,3.659-6.164l8.921-4.866  C30.073,41.417,31,39.854,31,38.155v-4.018v-0.001l-0.194-0.232l-0.038-0.045c-0.002-0.003-0.064-0.078-0.165-0.21  c-0.006-0.008-0.012-0.016-0.019-0.024c-0.053-0.069-0.115-0.152-0.186-0.251c-0.001-0.002-0.002-0.003-0.003-0.005  c-0.149-0.207-0.336-0.476-0.544-0.8c-0.005-0.007-0.009-0.015-0.014-0.022c-0.098-0.153-0.202-0.32-0.308-0.497  c-0.008-0.013-0.016-0.026-0.024-0.04c-0.226-0.379-0.466-0.808-0.705-1.283c0,0-0.001-0.001-0.001-0.002  c-0.127-0.255-0.254-0.523-0.378-0.802l0,0c-0.017-0.039-0.035-0.077-0.052-0.116h0c-0.055-0.125-0.11-0.256-0.166-0.391  c-0.02-0.049-0.04-0.1-0.06-0.15c-0.052-0.131-0.105-0.263-0.161-0.414c-0.102-0.272-0.198-0.556-0.29-0.849l-0.055-0.178  c-0.006-0.02-0.013-0.04-0.019-0.061c-0.094-0.316-0.184-0.639-0.26-0.971l-0.091-0.396l-0.341-0.22  C26.346,25.803,26,25.176,26,24.5v-4c0-0.561,0.238-1.084,0.67-1.475L27,18.728V12.5v-0.354l-0.027-0.021  c-0.034-0.722,0.009-2.935,1.623-4.776C30.253,5.458,33.081,4.5,37,4.5c3.905,0,6.727,0.951,8.386,2.828  c1.947,2.201,1.625,5.017,1.623,5.041L47,18.728l0.33,0.298C47.762,19.416,48,19.939,48,20.5v4c0,0.873-0.572,1.637-1.422,1.899  l-0.498,0.153l-0.16,0.495c-0.669,2.081-1.622,4.003-2.834,5.713c-0.297,0.421-0.586,0.794-0.837,1.079L42,34.123v4.125  c0,1.77,0.983,3.361,2.566,4.153l9.553,4.776C56.513,48.374,58,50.78,58,53.457V55.5z"></path>
@@ -98,9 +101,10 @@
         </section>
       </div>
     </div>
+    <!--	4-->
     <div class="container-fuild">
       <div class="container py-2">
-        <div class="row mx-0">
+        <div class="row">
           <div class="col-12 col-sm-6 timemenedg_foto"></div>
           <div class="col-12 col-sm-6 timemenedg_notebook"><img class="col-10 col-sm-6" src="src/img/notebook.jpg">
             <p>
@@ -113,53 +117,91 @@
         </div>
       </div>
     </div>
+    <!--	5-->
     <div class="container-fuild">
       <div class="container">
         <div class="row mx-0 flex-column text-center">
           <h4>Key Features</h4>
           <h1 class="display-1">Designing for Positive Future</h1>
         </div>
-        <div class="row justify-content-between">
+        <div class="row mx-0 justify-content-between">
           <div class="col-12 col-sm-4">
-            <h4>Web Design</h4>
-            <p class="text-muted">Suspendisse vulputate eros in velit fermentum cursus. Nulla fringilla justo tellus, at sodales eros laoreet a. Quisque facilisis lorem augue, sit amet</p>
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Web Design</h4>
+                  <p class="card-text text-muted">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-12 col-sm-4">
-            <h4>Development</h4>
-            <p class="text-muted">Suspendisse vulputate eros in velit fermentum cursus. Nulla fringilla justo tellus, at sodales eros laoreet a. Quisque facilisis lorem augue, sit amet</p>
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Development</h4>
+                  <p class="card-text text-muted">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-12 col-sm-4">
-            <h4>Photography</h4>
-            <p class="text-muted">Suspendisse vulputate eros in velit fermentum cursus. Nulla fringilla justo tellus, at sodales eros laoreet a. Quisque facilisis lorem augue, sit amet</p>
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Photography</h4>
+                  <p class="card-text text-muted">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="row justify-content-between mt-2">
+        <div class="row mx-0 justify-content-between">
           <div class="col-12 col-sm-4">
-            <h4>Branding</h4>
-            <p class="text-muted">Suspendisse vulputate eros in velit fermentum cursus. Nulla fringilla justo tellus, at sodales eros laoreet a. Quisque facilisis lorem augue, sit amet</p>
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Branding</h4>
+                  <p class="card-text text-muted">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-12 col-sm-4">
-            <h4>Marketing</h4>
-            <p class="text-muted">Suspendisse vulputate eros in velit fermentum cursus. Nulla fringilla justo tellus, at sodales eros laoreet a. Quisque facilisis lorem augue, sit amet</p>
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Marketing</h4>
+                  <p class="card-text text-muted">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-12 col-sm-4">
-            <h4>Support</h4>
-            <p class="text-muted">Suspendisse vulputate eros in velit fermentum cursus. Nulla fringilla justo tellus, at sodales eros laoreet a. Quisque facilisis lorem augue, sit amet</p>
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Support</h4>
+                  <p class="card-text text-muted">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <!--	6-->
     <div class="container-fuild">
       <div class="container px-0">
-        <div class="row justify-content-between my-3">
+        <div class="row mx-0 justify-content-between my-3">
           <div class="col-12 col-sm-4">
-            <div class="pers_1"></div>
+            <div class="pers-1"></div>
           </div>
           <div class="col-12 col-sm-4">
-            <div class="pers_2"></div>
+            <div class="pers-2"></div>
           </div>
           <div class="col-12 col-sm-4">
-            <div class="pers_3"></div>
+            <div class="pers-3"></div>
           </div>
         </div>
         <div class="row mx-0 py-2">
@@ -171,6 +213,7 @@
         </div>
       </div>
     </div>
+    <!--	7-->
     <div class="container-fuild">
       <section class="container">
         <div class="row mx-0 flex-column text-center mb-3">
@@ -188,6 +231,7 @@
         </div>
       </section>
     </div>
+    <!--	8-->
     <div class="container-fuild">
       <div class="container">
         <div class="row">
@@ -214,6 +258,7 @@
         </div>
       </div>
     </div>
+    <!--	9-->
     <div class="container-fuild">
       <div class="container">
         <div class="row mx-0 justify-content-around">
@@ -263,7 +308,28 @@
         </div>
       </div>
     </div>
-    <div class="container-fuild"></div>
+    <!--	10-->
+    <div class="container-fuild">
+      <div class="container">
+        <div class="row mx-0 justify-content-around">
+          <div class="address_office col-12 col-sm-4">
+            <div class="address_office_svg"><span>Headline</span>
+              <p>Sample footer text</p>
+            </div>
+          </div>
+          <div class="number_phono col-12 col-sm-4">
+            <div class="number_phono_svg"><span>Headline</span>
+              <p>Sample footer text</p>
+            </div>
+          </div>
+          <div class="email col-12 col-sm-4">
+            <div class="email_svg"><span>Headline</span>
+              <p>Sample footer text</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="gulp/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
